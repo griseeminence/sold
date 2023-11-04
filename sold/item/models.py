724b5abd542ec7c45d5a83ee0b9d8e3e06f95390
griseeminence <p.kolt080@gmail.com>
2023-11-04 21:49:59 +0800

    add categories and new item-models

---
+++ sold/item/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+User = get_user_model()
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'categories'
+        verbose_name = 'category'
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
