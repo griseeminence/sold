f248ba1178442a55be2a7ee458610f220330bcf2
griseeminence <p.kolt080@gmail.com>
2023-11-05 16:24:09 +0800

    add login form and html page

--- sold/core/urls.py
+++ sold/core/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import index, contact, signup
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm
+
+
 
 app_name = 'core'
 
@@ -8,4 +12,9 @@ urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('signup/', signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(
+        authentication_form=LoginForm,
+        template_name='core/login.html'
+    ), name='login'),
+
 ]
--- sold/core/views.py
+++ sold/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
 from .forms import SignupForm
 
@@ -20,7 +20,13 @@ def contact(request):
 
 
 def signup(request):
-    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
     context = {
         'form': form
     }
