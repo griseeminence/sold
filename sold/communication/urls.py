130c5b2056a341b6f3fcf8f0cbc87d413557ecdd
griseeminence <p.kolt080@gmail.com>
2023-11-06 18:31:39 +0800

    procces of communication between cust and sell

--- sold/communication/urls.py
+++ sold/communication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import reverse_lazy, path
-from .views import new_communication
+from .views import new_communication, inbox
 
 app_name = 'communication'
 
 urlpatterns = [
+    path('inbox/', inbox, name='inbox'),
     path('inbox/new/<int:item_pk>/', new_communication, name='new_communication'),
 ]
--- sold/communication/views.py
+++ sold/communication/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Communication
@@ -5,19 +6,23 @@ from item.models import Item
 
 from .forms import CommunicationMessageForm
 
-# Create your views here.
 
+# Create your views here.
+@login_required
 def new_communication(request, item_pk):
     item = get_object_or_404(Item, pk=item_pk)
+
     if item.created_by == request.user:
         return redirect('dashboard:index')
 
     communications = Communication.objects.filter(item=item).filter(members__in=[request.user.id])
+
     if communications:
         pass
 
     if request.method == 'POST':
         form = CommunicationMessageForm(request.POST)
+
         if form.is_valid():
             communication = Communication.objects.create(item=item)
             communication.members.add(request.user)
@@ -30,6 +35,24 @@ def new_communication(request, item_pk):
             communication_message.save()
 
             return redirect('item:detail', pk=item_pk)
-        else:
-            form = CommunicationMessageForm()
-        return render(request, 'communication/new_communication.html', {'form': form})
+    else:
+        form = CommunicationMessageForm()
+
+    return render(request, 'communication/new_communication.html', {
+        'form': form
+    })
+
+@login_required
+def inbox(request):
+    communications = Communication.objects.filter(members__in=[request.user.id])
+    return render(request, 'communication/inbox.html', {
+        'communications': communications
+    })
+
+
+@login_required
+def detail(request, pk):
+    communication = Communication.objects.filter(members__in=[request.user.id].get(pk=pk))
+    return render(request, 'communication/detail.html', {
+        'communication': communication
+    })
