f0e29ebf4bf43d9ff7cfb4e96af31213276b6dc4
griseeminence <p.kolt080@gmail.com>
2024-01-06 12:40:35 +0100

    v2.12

--- sold/core/forms.py
+++ sold/core/forms.py
@@ -35,4 +35,13 @@ class SignupForm(UserCreationForm):
 #     password = forms.CharField(widget=forms.PasswordInput(attrs={
 #         'placeholder': 'Your password',
 #         'class': 'w-full py-4 px-6 rounded-xl'
-#     }))
+#     }))# class LoginForm(AuthenticationForm):
+# #     username = forms.CharField(widget=forms.TextInput(attrs={
+# #         'placeholder': 'Your username',
+# #         'class': 'w-full py-4 px-6 rounded-xl'
+# #     }))
+# #
+# #     password = forms.CharField(widget=forms.PasswordInput(attrs={
+# #         'placeholder': 'Your password',
+# #         'class': 'w-full py-4 px-6 rounded-xl'
+# #     }))
